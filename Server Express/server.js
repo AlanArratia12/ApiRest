a9bf670d01eb6ec548150831c32a9750f3e89dff
AlanArratia12 <ayah160403@hotmail.com>
2025-02-07 08:50:42 -0600

    Middleware y recepcion de datos

--- Server Express/server.js
+++ Server Express/server.js
@@ -1,11 +1,42 @@
 const express=require('express');
 const app=express();
 const port=3000;
-
-app.get('/',(req,res,next)=>{
+ 
+/*Middlewawre de Aplicacion
+app.use('/',(req,res,next)=>{
+console.log("Peticion al server");
+next();
+}, (req,res,next)=>{
+    console.log("2da funcion middlewawre");
+    next();
+}
+);
+ */
+ 
+// Middleware incorporado en express
+app.use(express.json());
+app.use(express.text());
+ 
+app.get('/alumnos', (req,res,next)=>{
+    console.log(req.query);
     res.sendFile(__dirname+'/index.html');
-});
-
+})
+ 
+app.post('/sistemas/:control', (req,res)=>{
+    console.log(req.params)
+    res.send("Metodo POST");
+})
+ 
+app.get('/maestros', (req,res)=>{
+    console.log(req.body)
+    res.send("Hola Mundo!");
+})
+ 
+app.use('/', (req,res,next)=>{
+    res.status(404);
+    res.send("Error 404");
+})
+ 
 app.listen(port,()=>{
-    console.log('server running at http://localhost:'+port);
+console.log('Server running at http://localhost:3000');
 });
