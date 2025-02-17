e489c5d99b09a941cd3607f95f265aa9e769093a
AlanArratia12 <ayah160403@hotmail.com>
2025-02-17 09:06:34 -0600

    Server Express actualizado 1702

--- Server Express/server.js
+++ Server Express/server.js
@@ -1,7 +1,12 @@
+xmlparser = require('express-xml-bodyparser');
 const express=require('express');
 const app=express();
 const port=3000;
- 
+const multer=require('multer');
+const path = require('path');
+
+
+
 /*Middlewawre de Aplicacion
 app.use('/',(req,res,next)=>{
 console.log("Peticion al server");
@@ -16,7 +21,21 @@ next();
 // Middleware incorporado en express
 app.use(express.json());
 app.use(express.text());
- 
+app.use(xmlparser());
+
+const folder = path.join(__dirname, '/archivos/'); 
+const upload = multer({ dest: folder });
+app.use(upload.single('archivo'));
+
+app.post('/prefecto', (req,res,next)=>{
+    //console.log(req.body);
+    //res.send("Metodo POST");
+    console.log(`Se recibio el archivo: ${req.file.originalname}`);
+    console.log(req.body);
+    console.log('Se recibio el formulario:'+JSON.stringify(req.body));
+    res.json(req.body);
+});
+
 app.get('/alumnos', (req,res,next)=>{
     console.log(req.query);
     res.sendFile(__dirname+'/index.html');
